fb6e5c9a457fbcf82b659292daffa1870db68321
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2024-12-19 21:58:27 +0200

    Update TaskItem.tsx

--- client/app/Components/TaskItem/TaskItem.tsx
+++ client/app/Components/TaskItem/TaskItem.tsx
@@ -28,7 +28,7 @@ function TaskItem({ task }: TaskItemProps) {
 
   return (
     <motion.div
-      className="h-[16rem] px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white"
+      className="h-auto px-4 py-3 flex flex-col gap-4 shadow-sm bg-[#f9f9f9] rounded-lg border-2 border-white"
       variants={item}
     >
       <div>
